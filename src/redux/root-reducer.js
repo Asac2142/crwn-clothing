05bb1e7a57cbaee48b8218ecc365f727773c7ef9
Andres Alban <bethesdas101@gmail.com>
2020-10-14 16:36:28 -0500

    Adding redux persist on cart items

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from 'redux';
-import CartReducer from './cart/cart.reducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //es el localStorage del Browser pero lo maneja redux-persist
 
+import CartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage, //storage: storage
+    whitelist: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: CartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
