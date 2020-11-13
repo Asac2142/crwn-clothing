7b995521303944d6abd5a8256608eec3254989a9
Andres <bethesdas101@gmail.com>
2020-11-13 12:05:46 -0500

    Adding shop and directory reducers

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,8 @@ import storage from 'redux-persist/lib/storage'; //es el localStorage del Browse
 
 import CartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +15,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
